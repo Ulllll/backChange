9c6890cb91639e349c90872433179b2952fa5dff
Ulllll <nooryes1@gmail.com>
2019-06-05 14:26:23 +0300

    background change

---
+++ customLayout/CustomViewCell.m
@@ -0,0 +1,55 @@
+//
+//  CustomViewCell.m
+//  customLayout
+//
+//  Created by Анастасия Рябова on 02/06/2019.
+//  Copyright © 2019 Анастасия Рябова. All rights reserved.
+//
+
+#import "CustomViewCell.h"
+
+@interface CustomViewCell()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@end
+
+@implementation CustomViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        _titleLabel = [UILabel new];
+        _titleLabel.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.8];
+        //_titleLabel.backgroundColor = [UIColor orangeColor];
+        [self.contentView addSubview:_titleLabel];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    self.titleLabel.frame = CGRectMake(20.f, 20.f, 140.f, 140.f);
+
+}
+
+- (void)preapeForReuse
+{
+    [super prepareForReuse];
+    
+    self.titleLabel = nil;
+}
+
+- (void)setTitle:(NSString *)title
+{
+    self.titleLabel.text = title;
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    self.titleLabel.font = [UIFont fontWithName:@"Avenir-BookOblique" size: 30.f];
+}
+
+
+@end
---
+++ customLayout/ViewController.m
@@ -0,0 +1,86 @@
+//
+//  ViewController.m
+//  customLayout
+//
+//  Created by Анастасия Рябова on 02/06/2019.
+//  Copyright © 2019 Анастасия Рябова. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CustomViewCell.h"
+
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate>
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, copy) NSArray<NSString *> *moreTitle;
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UICollectionViewFlowLayout *collectionViewLayout = [UICollectionViewFlowLayout new];
+    collectionViewLayout.minimumInteritemSpacing = 10.f;
+    collectionViewLayout.minimumLineSpacing = 10.f;
+    collectionViewLayout.itemSize = CGSizeMake(180.f, 180.f);
+    collectionViewLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:collectionViewLayout];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.collectionView];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    
+    self.moreTitle = @[@"First", @"Second", @"Third", @"Four", @"Five", @"Six", @"Seven", @"Eight", @"Nine"];
+   
+    [self.collectionView registerClass:[CustomViewCell class] forCellWithReuseIdentifier:NSStringFromClass([CustomViewCell class])];
+    
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    self.collectionView.frame = CGRectMake(0.f, 0.f, CGRectGetWidth(self.view.bounds), CGRectGetWidth(self.view.bounds)+CGRectGetWidth(self.view.bounds));
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.moreTitle.count;
+}
+
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    CustomViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([CustomViewCell class]) forIndexPath:indexPath];
+    
+    NSString *title = self.moreTitle[indexPath.row];
+    
+    cell.backgroundColor = [UIColor blueColor];
+    cell.title = title;
+    
+    collectionView.allowsMultipleSelection = NO;
+    collectionView.allowsSelection = YES;
+    [collectionView selectItemAtIndexPath:indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor yellowColor];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor blueColor];
+}
+
+
+@end
